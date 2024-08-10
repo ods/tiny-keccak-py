0ff96f08d914069e5c0eced5323fad4ec1d86416
Denis Otkidach <denis.otkidach@gmail.com>
2024-08-10 10:41:09 +0300

    Update to work with maturin 1.*

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,11 @@ use tiny_keccak::{Hasher, Keccak};
 
 /// The Keccak-256 hash function defined in Keccak SHA3 submission
 #[pyfunction]
-fn keccak256<'a>(py: Python<'a>, input: &[u8]) -> PyResult<&'a PyBytes> {
+fn keccak256<'py>(py: Python<'py>, input: &[u8]) -> PyResult<Bound<'py, PyBytes>> {
     let mut hasher = Keccak::v256();
     hasher.update(input);
-    PyBytes::new_with(py, 32, |mut output: &mut [u8]| {
-        hasher.finalize(&mut output);
+    PyBytes::new_bound_with(py, 32, |output: &mut [u8]| {
+        hasher.finalize(output);
         Ok(())
     })
 }
@@ -15,7 +15,7 @@ fn keccak256<'a>(py: Python<'a>, input: &[u8]) -> PyResult<&'a PyBytes> {
 /// The Keccak-256 hash function defined in Keccak SHA3 submission
 #[pymodule]
 #[pyo3(name = "tiny_keccak")]
-fn tiny_keccak_py(_py: Python, m: &PyModule) -> PyResult<()> {
+fn tiny_keccak_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(keccak256, m)?)?;
     Ok(())
 }
